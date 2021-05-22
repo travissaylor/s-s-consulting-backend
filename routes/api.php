8db1f30086cf4f65f846ee21d4817d878ee39228
Travis Saylor <tsaylor@Traviss-MBP.attlocal.net>
2021-05-21 19:54:08 -0500

    First Commit for backend Api including Sanctum & Fortify. Basic Project CRUD working w/o auth

---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\ProjectController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::apiResource('projects', ProjectController::class);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    
+});
+
