b3cde023e4ce70fcf4fec2999aa4d7622b65cd7e
Travis Saylor <tsaylor@Traviss-MBP.attlocal.net>
2021-05-29 11:55:19 -0500

    Add auth to sensitive endpoints

--- routes/api.php
+++ routes/api.php
@@ -15,13 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::apiResource('projects', ProjectController::class);
+Route::get('/projects', [ProjectController::class, 'index']);
+Route::get('/projects/{id}', [ProjectController::class, 'show']);
+Route::put('/projects/{id}', [ProjectController::class, 'update']);
+
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
 
-    
+    Route::post('/projects', [ProjectController::class, 'store']);
+    Route::delete('/projects/{id}', [ProjectController::class, 'destroy']);
 });
 
